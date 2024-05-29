1c92dfdb59423174cafe3b9f7298199ca9cbc34b
BrandonFrei <brandon.freiberger98@gmail.com>
2024-05-29 14:50:39 -0700

    Fixed ordering in exports to align with cx_map in kernel cx_sys.c

--- zoo/exports.c
+++ zoo/exports.c
@@ -5,14 +5,14 @@
 int32_t (*cx_func_error[]) (int32_t, int32_t, int32_t) = {0};
 
 cx_func_stub_t cx_funcs[MAX_CX_ID] = {
-    cx_func_addsub,
     cx_func_muldiv,
+    cx_func_addsub,
     cx_func_mulacc
     };
 
 int32_t num_cfs[MAX_CX_ID] = {
-    CX_ADDSUB_NUM_FUNCS,
     CX_MULDIV_NUM_FUNCS, 
+    CX_ADDSUB_NUM_FUNCS,
     CX_MULACC_NUM_FUNCS
     };
 
