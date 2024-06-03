898106ad6aa4afab3ddd2189180f22c51e4e583a
BrandonFrei <brandon.freiberger98@gmail.com>
2024-06-02 23:28:53 -0700

    Removed dependency on libci.h in qemu

--- utils/build-riscv-toolchain.sh
+++ utils/build-riscv-toolchain.sh
@@ -115,7 +115,7 @@ pushd build
 export LIBRARY_PATH=$RISCV/../build-qemu/lib
 export LD_LIBRARY_PATH=$RISCV/../build-qemu/lib
 
-../configure --target-list=riscv64-linux-user,riscv32-linux-user,riscv64-softmmu,riscv32-softmmu
+../configure --target-list=riscv32-softmmu
 
 if [ $? -ne 0 ]; then
 	echo "issue with configuring qemu - possibly due to not having a python venv active"
