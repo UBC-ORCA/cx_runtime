1ecae88fa74ef72cbabc2fdd5752ec7996871081
BrandonFrei <brandon.freiberger98@gmail.com>
2025-06-12 13:16:50 -0700

    Unset LD_LIBRARY when building toolchain

--- utils/build-riscv-toolchain.sh
+++ utils/build-riscv-toolchain.sh
@@ -4,6 +4,9 @@
 # NOTE: This is a very slow build (2h+) due to gnu toolchain
 #####
 
+unset LIBRARY_PATH
+unset LD_LIBRARY_PATH
+
 pushd $CX_ROOT/utils/riscv-gnu-toolchain
 
 sudo apt-get install autoconf automake autotools-dev curl python3 python3-pip python3-tomli libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
