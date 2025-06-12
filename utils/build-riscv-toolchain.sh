0ec1ebc1e5cc7eaf2093826957b58d779f747adb
BrandonFrei <brandon.freiberger98@gmail.com>
2025-06-12 12:34:23 -0700

    Fixed typo in toolchain build script

--- utils/build-riscv-toolchain.sh
+++ utils/build-riscv-toolchain.sh
@@ -40,7 +40,7 @@ fi
 
 mkdir build
 pushd build
-../configure --prefix=$CX2_ROOT/utils/riscv --with-arch=rv32imav --with-abi=ilp32
+../configure --prefix=$CX_ROOT/utils/riscv --with-arch=rv32imav --with-abi=ilp32
 
 if [ $? -ne 0 ]; then
 	echo "issue with configuring gnu build"
